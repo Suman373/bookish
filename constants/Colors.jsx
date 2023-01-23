41609e2f8aad6db3d3e4230b9e9ab7dfeb309b56
Suman Roy <95040233+Suman373@users.noreply.github.com>
2023-01-23 20:50:28 +0530

    add: stepzen integration, constant colors

---
+++ constants/Colors.jsx
@@ -0,0 +1,16 @@
+export default{
+    light:{
+        text:'#000',
+        background:'#fff',
+        tint:"#2f95dc",
+        tabIconDefault:"#ccc",
+        tableIconSelected: "#fff"
+    },
+    dark:{
+        text:"#fff",
+        background:"000",
+        tinit:"#2f95dc",
+        tabIconDefault:"#ccc",
+        tableIconSelected:"#fff"
+    }   
+};
