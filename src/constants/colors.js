1a4acc0e5a31cbb2ca3e8a78af26e59273445595
Suman Roy <95040233+Suman373@users.noreply.github.com>
2023-01-24 18:50:16 +0530

    add: constants/routes added, navigation installed

---
+++ src/constants/colors.js
@@ -0,0 +1,23 @@
+export default{
+    light:{
+        text:'#000000',
+        background:'#ffffff',
+        tint:"#2f95dc",
+        tabIconDefault:"#cccccc",
+        tableIconSelected: "#ffffffff"
+    },
+    dark:{
+        text:"#ffffff",
+        background:"#000000",
+        tinit:"#2f95dc",
+        tabIconDefault:"#ccc",
+        tableIconSelected:"#ffffff"
+    },
+    gradientForm: "#A376f1",
+    primary:"#7d5fff",
+    white:"#ffffff",
+    bgColor:"#82ccdd",
+    warning:"#f0d500",
+    danger:"#ff0d0e",
+    success:"green",
+};
